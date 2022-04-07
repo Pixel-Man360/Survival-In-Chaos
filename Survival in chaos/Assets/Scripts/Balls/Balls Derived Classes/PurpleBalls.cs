32e9d3de3dae46e8f2fe245213388d6c1c0386dd
Pixel-Man360 <muntasir.chowdury360@gmail.com>
2022-04-07 12:10:42 +0600

    Purple Ball Dummy Code Added

---
+++ Survival in chaos/Assets/Scripts/Balls/Balls Derived Classes/PurpleBalls.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PurpleBalls : Balls, IReturnObject
+{
+    bool firstHit = false;
+    bool secondHit = false;
+    bool thirdHit = false;
+    
+    protected override void HandleCollisions(Collision2D other)
+    {
+        if(other.gameObject.CompareTag("Bullet"))
+        {
+            BallHitBullet();
+        }
+
+        else if(other.gameObject.CompareTag("Player"))
+        {
+            BallHitPlayer();
+        }
+
+        else if(other.gameObject.CompareTag("Walls"))
+        {
+            StartCoroutine(BallHitWall(this.gameObject, 5f));
+        }
+    }
+
+    protected override void BallHitPlayer()
+    {
+        _ballDamage = 50;
+        base.BallHitPlayer();
+        StartCoroutine(ReturnObj());
+    }
+
+    protected override void BallHitBullet()
+    {
+        // CheckBlastRadiusForPlayer();
+
+        // base.BallHitBullet();
+        // StartCoroutine(ReturnObj());
+
+        //Tripple Shot Kill
+    }
+
+    public IEnumerator ReturnObj()
+    {
+        yield return new WaitForSeconds(0.5f);
+        ObjectPool.instance.ReturnToPool(this.gameObject);
+    }
+}
